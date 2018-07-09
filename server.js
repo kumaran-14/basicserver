bce8cd2ea525969641dad351f89c82a2d5f9a8da
Kumaran <kumarangowrisankar@yahoo.in>
2018-07-09 15:58:01 +0530

    Basic Server For Testing

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
+
+//mongo
+// const MongoClient = require('mongodb').MongoClient
+
+// MongoClient.connect('mongodb://kumaran-14:lakshmi2010@ds261450.mlab.com:61450/hahas', (err, client) => {
+//   if(err) console.log(err);
+//   db = client.db('hahas')
+//   app.listen(3000,() => {
+//     console.log("SERVER fired up")
+//   })
+// })
+
+app.set('views', path.join(__dirname,"views"))
+app.set('view engine','pug')
+
+//route handlers
+
+app.listen(3000,() => {
+  console.log('SERVER STARTED. LISTENEING ON PORT 3000')
+})
